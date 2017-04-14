86ee5dc079c6c95003d356dcf170399f5a804884
Paulo Corpuz <akocpauloneil@yahoo.com>
2017-04-13 22:35:08 -0500

    webhook

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
-	
-	if(isset($_REQUEST['hub_challenge'])){
-		$challenge = $_REQUEST['hub_challenge'];
-		$token = $_REQUEST['hub_verify_token'];
-	}
-	
-	if($token == "myCustomToken123"){
-		echo $challenge;
-		
-	}
-	
-	?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	
-	
-</body>
-</html>
+
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+
+if ($verify_token === 'my_token_code') {
+echo $challenge;
+}
+
+?>
