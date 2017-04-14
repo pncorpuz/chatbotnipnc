83fb01bb761bd14d3eb71a7ddd41da92a6baa730
Paulo Corpuz <akocpauloneil@yahoo.com>
2017-04-13 22:26:48 -0500

    changed

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<?php
+<?php
 	
 	if(isset($_REQUEST['hub_challenge'])){
 		$challenge = $_REQUEST['hub_challenge'];
@@ -19,6 +11,15 @@
 	}
 	
 	?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	
 	
 </body>
 </html>
